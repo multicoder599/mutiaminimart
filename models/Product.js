15d8a9b0a214909412da78923393cb81d115b97d
Newton Multi <multinewton@gmail.com>
2026-08-30 22:07:33 +0300

    s

--- models/Product.js
+++ models/Product.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    type: { type: String, required: true },
-    price: { type: Number, required: true },
+    barcode: { type: String, unique: true, sparse: true },
+    type: { type: String, default: 'others' },
+    price: { type: Number, default: 0 },
     buying_price: { type: Number, default: 0 },
-    stock: { type: Number, default: 0 }
+    stock: { type: Number, default: 0 },
+    image: { type: String, default: null }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
